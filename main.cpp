5675f1373d58d404a62b14eb9b06c5146a1a398b
marchel99 <kubamarchelski@live.com>
2023-04-14 22:42:38 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -98,3 +98,116 @@ ostream& operator<<(ostream& os, complex& c) {
 	else {
 		os << " - ";
 	}
+
+	os << "j" << imag;
+
+	return os;
+}
+
+ostream& operator<<(ostream& os, complex&& c) {
+	os << c.get_real();
+
+	double imag = c.get_imag();
+
+	if (imag >= 0.0) {
+		os << " + ";
+	}
+	else {
+		os << " - ";
+	}
+
+	os << "j" << imag;
+
+	return os;
+}
+
+// polimorfizm
+// poj�cie pod kt�rym kryje si� przeci��anie nazw i operator�w
+// funkcje musz� mie� inne nag��wki, aby kompilator wiedzia�,
+// kt�ra funkcja ma by� wywo�ana
+
+void fun1(int& i) {
+	i += 10;
+	cout << "Zmodyfikowalem zmienna\n";
+}
+
+//void fun1(int& j) {
+// nag��wek taki sam jak w definicji powy�ej
+//}
+
+//void fun1(int i) {
+// nag��wek nie jest co prawda dok�adnie taki sam, ale kompilator nie b�dzie
+// w stanie wybra� odpowiedniej funkcji bo obydwie funkcje da si� wywo�a� 
+// ze zmienn� typu int
+//}
+
+void fun1() {
+	cout << "Druga wersja funkcji\n";
+}
+
+int main() {
+	complex a; // konstruktor domy�lny
+	complex b(5, 5); // konstruktor z parametrami
+
+	if (true) {
+		complex c(1, 2);
+		// jaki� kod 
+	} // koniec �ycia zmiennej c
+
+	int i = 5;
+	int* pt_i = nullptr; // wska�nik typu int
+	int& ref = i; // wymaga inicjalizatora
+
+	pt_i = &i; // wska�nik przechowuje adres zmiennej i
+
+	// spos�b stosowania
+
+	//operator<<(cout, a); 
+
+	cout << a << endl; // cout << a; zwraca -> cout 
+			           // cout << endl;
+
+	cout << "Zmienna o adresie " << &i << " przechowuje wartosc " << i << endl;
+	cout << "Zmienna wskaznikowa o adresie " << &pt_i << " przechowuje wartosc " << pt_i << endl;
+	cout << "Zmienna referencyjna o adresie " << &ref << " przechowuje wartosc " << ref << endl;
+
+	// adres zmiennej referencyjnej i orygina�u jest dok�adnie taki sam
+	// oznacza to, �e jest to jedna i ta sama zmienna pod dwiema r�nymi nazwami
+
+	fun1(i);
+
+	cout << "Wartosc zmiennej po wywolaniu funkcji: " << i << endl;
+
+	fun1();
+
+	int x = 1, y = 2;
+
+	cout << x + y << endl; // z "x + y" pozostaje wyra�enie typu int, kt�re ma warto�� x + y
+
+	cout << "Liczba a: " << a << endl;
+
+	cout << "Liczba b: " << b << endl;
+
+	cout << "a + b: " << a + b << endl;
+
+	cout << "a - b: " << a - b << endl;
+
+	a += b;
+
+	cout << "zmienna a po operacji += : " << a << endl;
+
+	cout << "a + b: " << a + b << endl;
+
+	simple_vector array(10); // tworzymy "wektor" 10 liczb typu double
+
+	for (int i = 0; i < 11; ++i) {
+		try {
+			// operator[](int i) 
+			array[i] = i * 2 + 1; // 2x + 1, zapis do element�w tablicy
+			cout << "2 * " << i << " + 1 = " << array[i] << endl; // odczyt z tablicy
+		}
+		catch (const char* msg) {
+			cout << msg << endl;
+		}
+	}
+}

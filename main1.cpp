977ed9b49031c28e6b00c9c7aab39002327c767b
marchel99 <kubamarchelski@live.com>
2023-04-15 14:51:43 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   main1.cpp
    	modified:   main1.exe

--- main1.cpp
+++ main1.cpp
@@ -3,21 +3,77 @@
 // #define log(x) std::cout << x << std::endl
 using namespace std;
 
+class Ksiazka
+{ // ksiazka to obiejkt
+public:
+    int strony; // atrybuty klasy
+    char autor;
+
+    void otworz();
+};
+
+class Auto
+{
+public:
+    string marka;
+    string model;
+    unsigned int rocznik;
+    unsigned int przebieg;
+
+    void wczytaj();
+    void wypisz();
+};
+
+void Ksiazka::otworz()
+{
+    std::cout << "Otwieram.." << std::endl;
+}
+
+void Auto::wczytaj()
+{
+    std::cout << "Wczytuje.."  <<std::endl;
+}
+
+void Auto::wypisz()
+{
+    std::cout << "Wypisuje.." << std::endl;
+}
+
 int main()
 {
-    int a = 0;
-    int b=0;
 
-    cout << "Ile masz a" << endl;
+    Auto peugot;
+    peugot.marka = "peugot";
+    peugot.model = "508";
+    peugot.rocznik=2004;
+    peugot.przebieg=196542;
 
-    cin >> a;
+string a;
+cout << "Podaj nazwe obiektu" << endl;
+ cin >> a;
 
-    cout <<"Ile masz b" <<endl;
+  if (a.compare("peugot") == 0) {
+    Auto car;
+    car.wczytaj();
+  }
 
-    cin >> b;
+    /*
+        int a = 0;
+        int b = 0;
 
-    cout << "a:" << a << endl << "b:" <<b <<endl;
+        cout << "Ile masz a" << endl;
 
+        cin >> a;
+
+        cout << "Ile masz b" << endl;
+
+        cin >> b;
+
+        int &a_ref = a;
+
+        cout << "a:" << a << endl
+             << "b:" << b << endl;
+     */
     // podajemy jakiego typu jest to odniesienie
     // nazwa odniesienia jest ze slowa "reference"
     //  odniesienie odnosi się do zmiennej b

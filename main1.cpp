10d493cb06d3478ad8d917b3096842c7e18caefd
marchel99 <kubamarchelski@live.com>
2023-04-15 13:55:44 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   main1.cpp
    	modified:   main1.exe

--- main1.cpp
+++ main1.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 
-int main() {
+// #define log(x) std::cout << x << std::endl
+using namespace std;
 
-int i=5;
-int* pt_1=
+int main()
+{
+    int a = 0;
+    int b=0;
 
+    cout << "Ile masz a" << endl;
 
-    std::cout << "Hello, World!" << std::endl;
+    cin >> a;
+
+    cout <<"Ile masz b" <<endl;
+
+    cin >> b;
+
+    cout << "a:" << a << endl << "b:" <<b <<endl;
+
+    // podajemy jakiego typu jest to odniesienie
+    // nazwa odniesienia jest ze slowa "reference"
+    //  odniesienie odnosi się do zmiennej b
+
+    /*
+    int b=5;
+    int& refb=b;
+    refb=232;
+
+     */
+
     return 0;
 }

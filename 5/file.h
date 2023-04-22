68d4625f9d17dd272b790866f5cf90704d63e317
marchel99 <kubamarchelski@live.com>
2023-04-22 14:07:05 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   5/file.h

--- 5/file.h
+++ 5/file.h
@@ -5,9 +5,15 @@ using namespace std;
 
 class Book
 {
+    string tytul;
+    string wydawnictwo;
+    string autor;
+    int rok_wydania;
+    int ilosc_stron;
+public:
+    void get();
+    void set();
 
-
-    
 }
 
 
